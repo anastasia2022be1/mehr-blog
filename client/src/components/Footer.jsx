a97ac188605e5b38b3a162edb2a83cf61d83b1ab
Anastasiia Sevastianova <sevasyanova.anastasia1@gmail.com>
2024-11-25 21:28:38 +0100

    feat: add Footer with category links; add css to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <ul className="footer__categories">
+        <li><Link to="/posts/categories/Travel">Travel</Link></li>
+        <li><Link to="/posts/categories/Fitness">Fitness</Link></li>
+        <li><Link to="/posts/categories/Food">Food</Link></li>
+        <li><Link to="/posts/categories/Parenting">Parenting</Link></li>
+        <li><Link to="/posts/categories/Beauty">Beauty</Link></li>
+        <li><Link to="/posts/categories/Photography">Photography</Link></li>
+        <li><Link to="/posts/categories/Art">Art</Link></li>
+        <li><Link to="/posts/categories/Writing">Writing</Link></li>
+        <li><Link to="/posts/categories/Music">Music</Link></li>
+        <li><Link to="/posts/categories/Book">Book</Link></li>
+      </ul>
 
-export default Footer
+      <div className="footer__copyright">
+        <small>All Rights Reserved &copy;</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
