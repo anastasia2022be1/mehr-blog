7dc527d43637c7d34d15bd065f7d728b423da8af
Anastasiia Sevastianova <sevasyanova.anastasia1@gmail.com>
2024-11-26 13:27:01 +0100

    feat: add data.js; refactor: change structure of the project- place of posts arr

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,50 +1,13 @@
 import { useState } from "react";
-import Travel1 from "../images/Travel1.jpg";
-import Travel2 from "../images/Travel2.jpg";
-import Travel3 from "../images/Travel3.jpg";
-import Travel4 from "../images/Travel4.jpg";
 import PostItem from "./PostItem.jsx";
+import { DUMMY_POSTS } from "../data.js";
 
-const DUMMY_POSTS = [
-  {
-    id: "1",
-    thumbnail: Travel1,
-    category: "travel",
-    title: "First post on this blog hhhhhhhhhhhhhhhhhhhhhhhhhh",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.hjgdsc  sbjasm skhjfenwfm  cxkjelkjrf,s sDLKJFQLKWNEFM <DSYCLKJWNDMSY Cjlksdfnsjdn elkjfenvm kjeslrg cxlvkjlsdnfs dslj,bn fa laa flka  jbjabse dkshfkaew  askjfhka dkjassnms djaknf b clalkanm fkejn   ",
-    authorID: 3,
-  },
-  {
-    id: "2",
-    thumbnail: Travel2,
-    category: "travel",
-    title: "Second post about mountains",
-    desc: "Explore the beauty of mountain adventures.",
-    authorID: 1,
-  },
-  {
-    id: "3",
-    thumbnail: Travel3,
-    category: "travel",
-    title: "City life and travel tips",
-    desc: "Discover how to make the most of your city tours.",
-    authorID: 2,
-  },
-  {
-    id: "4",
-    thumbnail: Travel4,
-    category: "travel",
-    title: "Beach vibes and relaxation",
-    desc: "Learn about the best beaches to visit this year.",
-    authorID: 4,
-  },
-];
 
 const Posts = () => {
   const [posts, setPosts] = useState(DUMMY_POSTS);
   return (
     <section className="posts">
-      <div className="container posts__container">
+      {posts.length > 0 ? <div className="container posts__container">
         {posts.map(({ id, thumbnail, category, title, desc, authorID }) => (
           <PostItem
             key={id}
@@ -56,7 +19,7 @@ const Posts = () => {
             authorID={authorID}
           />
         ))}
-      </div>
+      </div> : <h2 className="center">No posts founds</h2>}
     </section>
   );
 };
---
+++ client/src/data.js
@@ -0,0 +1,39 @@
+import Travel1 from "./images/Travel1.jpg";
+import Travel2 from "./images/Travel2.jpg";
+import Travel3 from "./images/Travel3.jpg";
+import Travel4 from "./images/Travel4.jpg";
+
+export const DUMMY_POSTS = [
+    {
+        id: "1",
+        thumbnail: Travel1,
+        category: "travel",
+        title: "First post on this blog hhhhhhhhhhhhhhhhhhhhhhhhhh",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.hjgdsc  sbjasm skhjfenwfm  cxkjelkjrf,s sDLKJFQLKWNEFM <DSYCLKJWNDMSY Cjlksdfnsjdn elkjfenvm kjeslrg cxlvkjlsdnfs dslj,bn fa laa flka  jbjabse dkshfkaew  askjfhka dkjassnms djaknf b clalkanm fkejn   ",
+        authorID: 3,
+    },
+    {
+        id: "2",
+        thumbnail: Travel2,
+        category: "travel",
+        title: "Second post about mountains",
+        desc: "Explore the beauty of mountain adventures.",
+        authorID: 1,
+    },
+    {
+        id: "3",
+        thumbnail: Travel3,
+        category: "travel",
+        title: "City life and travel tips",
+        desc: "Discover how to make the most of your city tours.",
+        authorID: 2,
+    },
+    {
+        id: "4",
+        thumbnail: Travel4,
+        category: "travel",
+        title: "Beach vibes and relaxation",
+        desc: "Learn about the best beaches to visit this year.",
+        authorID: 4,
+    },
+];
