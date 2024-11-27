0ac85b516a54e3573f5ea8ce86d823c687372df6
Anastasiia Sevastianova <sevasyanova.anastasia1@gmail.com>
2024-11-27 16:47:56 +0100

    connectin to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
-import express from "express"
+import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-// await connect(); // MongoDB
+// Подключение к MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("Failed to connect to MongoDB", err));
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log("Server started on port", port))
+app.listen(port, () => console.log(`Server started on port ${port}`));
