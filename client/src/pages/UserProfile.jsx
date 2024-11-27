73f91944145bc0303fc2bf4703b86cc99ba0ec39
Anastasiia Sevastianova <sevasyanova.anastasia1@gmail.com>
2024-11-27 12:51:32 +0100

    feat: add UserProfile and css

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Avatar from "../images/Avatar.png";
+import { FaEdit } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa";
 
 const UserProfile = () => {
+  const [avatar, setAvatar] = useState("");
   return (
-    <div>UserProfile</div>
-  )
-}
+    <section className="profile">
+      <div className="container profile__container">
+        <Link to={`/myposts/jsjsjjs`} className="btn">
+          My posts
+        </Link>
+        <div className="profile__details">
+          <div className="avatar__wrapper">
+            <div className="profile__avatar">
+              <img src={Avatar} alt="" />
+            </div>
+            <form className="avatar__form">
+              <input
+                type="file"
+                name="avater"
+                id="avatar"
+                onChange={(e) => setAvatar(e.target.files[0])}
+                accept="png, jpg, jpeg"
+              />
+              <label htmlFor="avatar">
+                <FaEdit />
+              </label>
+            </form>
+            <button className="profile__avatar-btn"><FaCheck /></button>
+          </div>
 
-export default UserProfile
+          <h1>Anastasia Sevastianova</h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default UserProfile;
