a6567e46ddc341e8d3679fdf15bd6da34807b991
Anastasiia Sevastianova <sevasyanova.anastasia1@gmail.com>
2024-11-28 11:15:39 +0100

    feat: get user(byId)

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -91,7 +91,18 @@ export const loginUser = async (req, res, next) => {
 // protected
 
 export const getUser = async (req, res, next) => {
-    res.json('User profile')
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id).select('-password');
+        if (!user) {
+            new HttpError('User not found', 404)
+        }
+
+        res.status(200).json(user)
+
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 //----------------------------------------------
