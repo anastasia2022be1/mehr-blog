8a7ac71fb0a1a7e7d1c283728e18a73e64064e0e
Anastasiia Sevastianova <sevasyanova.anastasia1@gmail.com>
2024-12-01 22:35:32 +0100

    feat: fetch to Get Users(All Authors)

--- client/src/pages/Authors.jsx
+++ client/src/pages/Authors.jsx
@@ -1,55 +1,38 @@
 import { Link } from 'react-router-dom'
-import { useState } from 'react';
-
-import Avatar1 from "../images/Avatar1.png";
-import Avatar2 from "../images/Avatar2.png";
-import Avatar3 from "../images/Avatar3.png";
-import Avatar4 from "../images/Avatar4.png";
-import Avatar5 from "../images/Avatar5.png";
-
-const authorsData = [
-  {
-    id: 1,
-    avatar: Avatar1,
-    name: "Jane Doe",
-    posts: 3,
-  },
-  {
-    id: 2,
-    avatar: Avatar2,
-    name: "John Smith",
-    posts: 5,
-  },
-  {
-    id: 3,
-    avatar: Avatar3,
-    name: "Emily Johnson",
-    posts: 2,
-  },
-  {
-    id: 4,
-    avatar: Avatar4,
-    name: "Michael Brown",
-    posts: 8,
-  },
-  {
-    id: 5,
-    avatar: Avatar5,
-    name: "Sophia Davis",
-    posts: 4,
-  },
-];
+import { useState, useEffect } from 'react';
+import Loader from '../components/Loader.jsx';
 
 const Authors = () => {
-  const [authors, setAuthors] = useState(authorsData);
+  const [authors, setAuthors] = useState([]); // Состояние для хранения авторов
+  const [isLoading, setIsLoading] = useState(false); // Состояние для индикатора загрузки
+  const [error, setError] = useState(null); // Состояние для ошибок
+
+
+  useEffect(() => {
+    const getAuthors = async () => {
+      setIsLoading(true)
+      try {
+        const response = await fetch(`http://localhost:5000/api/users`)
+        const data = await response.json(); // Получаем данные пользователей
+        setAuthors(data); // Сохраняем полученные данные
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false)
+    }
+    getAuthors(); // Вызов функции для загрузки данных
+  }, [])
+  if (isLoading) {
+    return <Loader />
+  }
   return (
     <section className="authors">
       {authors.length > 0 ? (
         <div className="container authors__container">
-          {authors.map(({ id, avatar, name, posts }) => (
+          {authors.map(({ _id:id, avatar, name, posts }) => (
             <Link key={id} to={`/posts/users/${id}`} className='author'>
               <div className="author__avatar">
-                <img src={avatar} alt={`${name}'s avatar`} />
+                <img src={`http://localhost:5000/uploads/${avatar}`} alt={`${name}'s avatar`} />
               </div>
                  <div className="author__info">
                   <h4>{name}</h4>
