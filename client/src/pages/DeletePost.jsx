8c5b8c00a47d1b1d5e9ff8f34e051fffbee4fce7
Anastasiia Sevastianova <sevasyanova.anastasia1@gmail.com>
2024-12-02 19:58:13 +0100

    feat: deletePost

--- client/src/pages/DeletePost.jsx
+++ client/src/pages/DeletePost.jsx
@@ -1,9 +1,11 @@
 import { useContext, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { UserContext } from '../context/userContext.jsx';
 
-const DeletePost = () => {
-    const navigate = useNavigate()
+const DeletePost = ({ postId }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   const { currentUser } = useContext(UserContext);
   const token = currentUser?.token;
 
@@ -11,9 +13,39 @@ const DeletePost = () => {
     if (!token) {
       navigate('/login')
     }
-  }, []);
+  }, [token, navigate]);
+
+  const removePost = async () => {
+    try {
+      const response = await fetch(`http://localhost:5000/api/posts/${postId}`, {
+        method: 'DELETE',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        },
+        credentials: 'include',
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Failed to delete the post');
+      }
+
+      if (location.pathname === `/myposts/${currentUser.id}`) {
+        navigate(0); // Обновление страницы
+      } else {
+        navigate('/'); // Переход на главную страницу
+      }
+    } catch (error) {
+      console.error('Error deleting post:', error.message);
+      alert(`Error: ${error.message}`);
+    }
+  };
+
   return (
-    <Link className='btn sm danger'>Delete</Link>
+    <Link className='btn sm danger' onClick={(e) => {
+        e.preventDefault(); 
+        removePost();
+      }}>Delete</Link>
   )
 }
 
